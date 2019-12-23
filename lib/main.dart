385e98b2157f3481a33fc554dc72ec443b092b34
Ata Mahmoud <ata.ata250@hotmail.com>
2019-12-24 01:19:41 +0200

    add themeing data

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,24 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
         accentColor: Colors.amber,
-        
+        fontFamily: 'QuickSand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          title: TextStyle(
+            fontFamily: 'OpenSans',
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+          ),
+          button: TextStyle(color: Colors.white),
+        ),
+        appBarTheme: AppBarTheme(
+          textTheme: ThemeData.light().textTheme.copyWith(
+            title: TextStyle(
+              fontFamily: 'OpenSans',
+              fontWeight: FontWeight.bold,
+              fontSize: 20
+            )
+          )
+        )
       ),
     );
   }
